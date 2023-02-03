4bc800feb095eccf6da852d331a5adf29112e457
plima94 <pauloalima1994@gmail.com>
2023-02-03 10:27:47 +0000

    added player

--- player.py
+++ player.py
@@ -10,4 +10,9 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
 
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
